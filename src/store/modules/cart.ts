001b60e23ea9ffd8a7840e764356d0077d90a80e
ernestorangel <ernestojrcjunior@gmail.com>
2025-07-18 15:33:28 -0300

    feat: implements basic cart store

---
+++ src/store/modules/cart.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+import type { CartItem } from '../../types'
+import { ref } from 'vue'
+
+export const useCartStore = defineStore('cart', () => {
+    const items = ref<CartItem[]>([])
+
+    function addItem(item: CartItem) {
+        items.value.push(item)
+    }
+
+    function removeItem(itemId: number) {
+        items.value = items.value.filter((item) => item.productId !== itemId)
+    }
+
+    function updateItemQuantity(productId: number, newQuantity: number) {
+        const item = items.value.find((item) => item.productId === productId);
+        if (item) {
+            item.quantity = newQuantity;
+        }
+    }
+
+    return { items, addItem, removeItem, updateItemQuantity}
+})
