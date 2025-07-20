8bd405deaa758680527ab48d5ef22b0b041d331d
ernestorangel <ernestojrcjunior@gmail.com>
2025-07-20 18:26:01 -0300

    feat: adds PWA manifest, icons, and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,32 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CPlug | Teste Vue',
+        short_name: 'CPlug',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#1c252d',
+        theme_color: '#1c252d',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
